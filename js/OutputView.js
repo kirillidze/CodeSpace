23fa451b29670a5d758b2b7085831d6e1e4a38d0
AlexKurta <41297214+AlexKurta@users.noreply.github.com>
2018-11-11 14:01:01 +0200

    Update OutputView.js

--- js/OutputView.js
+++ js/OutputView.js
@@ -5,11 +5,6 @@ export class OutputView {
 	constructor(model) {
 		this.myModel = model;
 		this.outputDoc = $('#OUTPUT')[0].contentWindow.document;
-		this.dataFromHtmlBlock = '';
-		this.isHead = null;
-		this.isBody = null;
-		
-
 		this.updateOutputHandler = this.updateOutput.bind(this);
 		this.toggleRunButtonHandler = this.toggleRunButton.bind(this);
 
@@ -33,43 +28,11 @@ export class OutputView {
 	updateOutput(data) {
 		this.outputDoc.head.innerHTML = '';		
 		this.outputDoc.body.innerHTML = '';
-		if (this.myModel.html || data.html)	{
-			this.dataFromHtmlBlock = this.myModel.html || data.html;		
-			this.isHead = this.dataFromHtmlBlock.match(/\<head\>[\s\S]*\<\/head\>/gim);
-			this.isBody = this.dataFromHtmlBlock.match(/\<body\>[\s\S]*\<\/body\>/gim);
-			
-			if (this.isHead) {
-				this.outputDoc.head.innerHTML = this.isHead[0].slice(6, -7); 
-			} 
-			if (this.isBody) {
-				this.outputDoc.body.innerHTML = this.isBody[0].slice(6, -7);
-			} 
-			if (!this.isBody && !this.isHead) {
-				this.outputDoc.body.innerHTML = this.dataFromHtmlBlock;
-			}
-		}
-		//при загрузке проекта обращаемся к аргументу,
-		//т.к. поля у модели к этому времени будут ещё null
-		//this.outputDoc.body.innerHTML = this.myModel.html || data.html;
-		
-		
-		//отображаем информацию в окне вывода
-		if(this.myModel.css || data.css) {
-			$('<style>', {
-				type: "text/css",
-				text: this.myModel.css || data.css
-			}).appendTo(this.outputDoc.head);
-		}		
-		//в некоторых браузерах alert срабатывает раньше, чем DOM
-		//даже если код будет в конце body, поэтому выполняем весь код
-		//с задержкой
-		if (this.myModel.js || data.js) {
-			$('<script>', {
-				type: "text/javascript",
-				text: `setTimeout( () => {${this.myModel.js || data.js}}, 500);`
-			}).appendTo(this.outputDoc.body);
-		}	
-		
+		this.outputDoc.write(`<html>
+                <head><style>${this.myModel.css || data.css}</style></head>
+                <body>${this.myModel.html || data.html}
+                <script>${this.myModel.js || data.js}<\/script>
+                </body></html>`);
 	}
 
 	unsubscribe() {
