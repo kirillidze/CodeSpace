0e6865682108e61780fa1184ed4486c8abbcfc26
AlexKurta <41297214+AlexKurta@users.noreply.github.com>
2018-11-15 11:51:45 +0200

    Update PopupView.js

--- js/PopupView.js
+++ js/PopupView.js
@@ -7,6 +7,8 @@ export class PopupView {
 		//подписываемся на изменения
 		this.myModel.changes.sub('changePopup', this.showPopup.bind(this));
 		this.myModel.changes.sub('hidePopup', this.hidePopup.bind(this));
+		this.myModel.changes.sub('changeUser', this.hidePopup.bind(this));
+		this.myModel.changes.sub('dataNotFound', this.showErrorMessage.bind(this));
 	}
 
 	showPopup(button) {
@@ -20,7 +22,11 @@ export class PopupView {
 	}
 
 	hidePopup() {
-		$('.layout, .popup, .popup__authorization, .popup__registration')
+		$('.layout, .popup, .popup__authorization, .popup__registration, .popup__error-message')
 			.css('display', 'none');
+	}
+
+	showErrorMessage() {
+		$('.popup__error-message').css('display', 'inline-block');
 	}
 }
