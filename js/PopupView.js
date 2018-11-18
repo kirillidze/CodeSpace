652ebe826a33783538fe4374199cd68d47d82cc3
AlexKurta <41297214+AlexKurta@users.noreply.github.com>
2018-11-18 14:49:19 +0200

    Update PopupView.js

--- js/PopupView.js
+++ js/PopupView.js
@@ -8,26 +8,34 @@ export class PopupView {
 		this.myModel.changes.sub('changePopup', this.showPopup.bind(this));
 		this.myModel.changes.sub('hidePopup', this.hidePopup.bind(this));
 		this.myModel.changes.sub('changeUser', this.hidePopup.bind(this));
-		this.myModel.changes.sub('dataNotFound', this.showErrorMessage.bind(this));
+		this.myModel.changes.sub('dataNotFound', this.showErrorLogIn.bind(this));
+		this.myModel.changes.sub('userWasFound', this.showErrorSignUp.bind(this));
 	}
 
 	showPopup(button) {
 		$('.layout, .popup').css('display', 'block');
 
 		if (button === 'logIn') {
-			$('.popup__authorization').css('display', 'block');
+			$('.popup__title_logIn').css('display', 'block');
+			$('.popup__button[value="Log In"]').css('display', 'inline-block');
 		} else if (button === 'signUp') {
-			$('.popup__registration').css('display', 'block');
+			$('.popup__title_signUp').css('display', 'block');
+			$('.popup__button[value="Sign Up"]').css('display', 'inline-block');
 		}
 	}
 
 	hidePopup() {
-		$('.layout, .popup, .popup__authorization, .popup__registration, .popup__error-message')
+		$('.layout, .popup, .popup__title, .popup__button_hide, .popup__error')
 			.css('display', 'none');
+		//очищаем поля
+		$('.popup__text-place')
+			.val('');
 	}
 
-	showErrorMessage() {
-		$('.popup__error-message').css('display', 'inline-block');
+	showErrorLogIn() {
+		$('.popup__error_logIn').css('display', 'inline-block');
+	}
+	showErrorSignUp() {
+		$('.popup__error_signUp').css('display', 'inline-block');
 	}
 }
-
