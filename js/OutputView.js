8b71f6f06ff48e9c4a21ce024c63650bd84ce46d
AlexKurta <41297214+AlexKurta@users.noreply.github.com>
2018-11-21 23:39:38 +0200

    Update OutputView.js

--- js/OutputView.js
+++ js/OutputView.js
@@ -3,70 +3,86 @@
 //представление окна ввывода данных уровня ПРОЕКТ (правая часть)
 export class OutputView {
 	constructor(model) {
-		this.myModel = model;
-		this.outputDoc = $('#OUTPUT')[0].contentWindow.document;
-
-		this.newHTML = '';
-		this.newScript = '';
-		this.newScriptArray = [];
-		this.scriptInHTML = [];
-		this.newCss = '';
-
+		this.myModel = model;		
 		this.updateOutputHandler = this.updateOutput.bind(this);
 		this.toggleAutoUpdateHandler = this.toggleAutoUpdate.bind(this);
-
 		//подписываемся на изменения
 		this.myModel.changes.sub('changeOnload', this.updateOutputHandler);
 		this.myModel.changes.sub('changeContent', this.updateOutputHandler);
 		this.myModel.changes.sub('changeAutoUpdate', this.toggleAutoUpdateHandler);
 	}
 
-	toggleAutoUpdate(autoUpdate) {
-		if (autoUpdate) {
+	toggleAutoUpdate(data) {
+		if (data.autoUpdate) {
 			//сразу обновляем
-			this.updateOutput();
+			this.updateOutput(data);
 		}
 	}
 
 	updateOutput(data) {
-		this.outputDoc.body.innerHTML = ''; //обнуляем body
-		this.outputDoc.head.innerHTML = ''; // обнуляем head
-		this.newScriptArray = [];
-		this.newHTML = this.myModel.html || data.html; // читаем поле html из модели
-		if (this.newHTML) {
-			//если в поле что-то есть, то пытаемся найти там script
-			this.scriptInHTML = this.newHTML
-				.match(/<script>[\S\s]+?<\/script>/gim);
-		}
-		if (this.scriptInHTML) {
-			// если скрипты найдены, удаляем их из html и собираем вместе
-			for (let i = 0; i < this.scriptInHTML.length; i++) {
-				this.newHTML = this.newHTML.split(this.scriptInHTML[i]).join('');
+		// заменяем Iframe на новый
+		$('<iframe>', {
+			id: "OUTPUT",
+			class: "main__ouput-window",
+			src: "",
+			sandbox: "allow-forms allow-modals allow-pointer-lock allow-popups allow-same-origin allow-scripts",
+			style: "display: block;"
+		}).replaceAll($('#OUTPUT'));		           
 
-				this.newScriptArray.push(this.scriptInHTML[i].slice(8, -9));
+		let outputDoc = $('#OUTPUT')[0].contentWindow.document,		
+			scriptInHTML = null,
+		 	newScriptArray = [],
+			newScript = '',
+			html = data.html;
+		if (html) {
+			//если в поле что-то есть, то пытаемся найти там script
+			scriptInHTML = html.match(/<script>[\S\s]+?<\/script>/gim);			
+		}
+		if (scriptInHTML) {
+			// если скрипты найдены, удаляем их из html и собираем вместе
+			for (let i = 0; i < scriptInHTML.length; i++) {
+				html = html.split(scriptInHTML[i]).join('');
+				newScriptArray.push(scriptInHTML[i].slice(8, -9));
 			}
 		}
-		// добавляем скрипт из поля JS
-		this.newScriptArray.push(this.myModel.js || data.js);
-		if (this.newScriptArray.length > 0) {
-			this.newScript = this.newScriptArray.join(';\n');
+		// добавляем данные скрипта JS
+		newScriptArray.push(data.js);
+		if (newScriptArray.length > 0) {
+			newScript = newScriptArray.join(';\n');
 		}
-		//убираем undefined
-		if (this.myModel.css || data.css) {
-			this.newCss = this.myModel.css || data.css;
-		} else this.newCss = '';
-
-		// записываем во фрейм то, что получилось
-		this.outputDoc.write(`<html>
-		<body>
-		<style>${this.newCss}</style>
-		${this.newHTML}
-		<script>
-		setTimeout( () => {${this.newScript}}, 500);
-		<\/script>
-		</body>
-		</html>`);
-
+		// ишем head
+		let headContent = html.match(/<head>[\S\s]+?<\/head>/gim),
+			headForIframe = null;
+		//если head есть, вырезаем его из тега	
+		if (headContent) {
+			headForIframe = headContent[0].slice(6, -7);
+		} else {		
+			headForIframe = '';
+		}
+		// ищем body
+		let bodyContent = html.match(/<body>[\S\s]+?<\/body>/gim),
+			bodyForIframe = null;
+		// если body есть, вырезаем его из тега
+		if (bodyContent) {
+			bodyForIframe = bodyContent[0].slice(6, -7);			
+		} else {
+		// если нет, оставляем html целиком	
+			bodyForIframe = html;
+		}
+		// вписываем результат в iframe
+		outputDoc.write(`<html>
+			<head>
+			${headForIframe} 			                          
+			<style>${data.css}</style>
+			</head>
+			<body>
+			${bodyForIframe}			
+			</body>
+			</html>`);
+		// добавляем наш script в конец body
+		setTimeout( () => {
+			$('<script>').text(newScript).appendTo(outputDoc.body);
+		}, 500);		
 	}
 
 	unsubscribe() {
@@ -78,5 +94,4 @@ export class OutputView {
 		this.myModel.changes
 			.remove('changeAutoUpdate', this.toggleAutoUpdateHandler);
 	}
-
 }
